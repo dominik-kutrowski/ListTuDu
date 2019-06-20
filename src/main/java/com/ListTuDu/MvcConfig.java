ad4c79137903032b4db8917328c847bc20e209ef
Dominik Kutrowski <dominik.kutrowski@gmail.com>
2019-06-21 00:19:18 +0200

    First Commit - update/comments no. 1

--- src/main/java/com/ListTuDu/MvcConfig.java
+++ src/main/java/com/ListTuDu/MvcConfig.java
@@ -4,37 +4,38 @@ import java.io.IOException;
 import com.sun.org.apache.bcel.internal.generic.GETFIELD;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@Configuration
+//@Configuration
 @Controller
 public class MvcConfig implements WebMvcConfigurer {
     //
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
+    //public void addViewControllers(ViewControllerRegistry registry) {
+        //registry.addViewController("/").setViewName("home");
         //registry.addViewController("/user").setViewName("user");
         //registry.addViewController("/home").setViewName("home");
         //registry.addViewController("/TuDu").setViewName("TuDu");
-    }
+    //}
 
+    @GetMapping(value={"/", "home"})
+    public String HomePage(Model modelhome) {
+        modelhome.addAttribute("HomePage", "Herzlich Willkommen!!");
+        return "home";
+    }
     @GetMapping("/user")
     public String UserPage(Model modeluser) {
         modeluser.addAttribute("UserPage", "info o Tobie!!");
-        return "onepagetest";
+        return "user";
     }
     @GetMapping("/TuDu")
     public String TuDuPage(Model modelTuDu) {
         modelTuDu.addAttribute("TuDuPage", "lista TuDu!!");
-        return "onepagetest";
-    }
-    @GetMapping("/home")
-    public String HomePage(Model modelhome) {
-        modelhome.addAttribute("HomePage", "Herzlich Willkommen!!");
-        return "onepagetest";
+        return "TuDu";
     }
 }
 
