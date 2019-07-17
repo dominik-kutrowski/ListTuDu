78c2374f1e10c5a22adf3de69fe71069a8e6bcb2
Dominik Kutrowski <dominik.kutrowski@gmail.com>
2019-07-17 23:33:24 +0200

    task no. 3 - delete task: with confirmation,

--- src/main/java/com/ListTuDu/ControllerTask.java
+++ src/main/java/com/ListTuDu/ControllerTask.java
@@ -8,14 +8,31 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
 @Controller
 public class ControllerTask  {
     @Autowired private TaskRepository taskRepository;
+    //
+    @PostMapping(value={"task/delete/{id}"})
+    public String TaskPageDelete(@PathVariable Long id){
+        taskRepository.deleteById(id);
+        return "redirect:/task/list";
+    }
+    @GetMapping(value={"task/delete/{id}"})
+    public String TaskPageDeleteQuestion(Model modelTaskDelete, @PathVariable Long id) {
+        //linijka ponizej albo nie dziala, albo nie wiem jak się do atrybutu TaskDelete dobrać w html :S
+        modelTaskDelete.addAttribute("TaskDelete", taskRepository.findById(id));
+        modelTaskDelete.addAttribute("TaskId", id);
+        return "task/delete";
+    }
+    //
+    //
+    @GetMapping("task")
+    public String TaskPage(Model modelTask) {
+        return "task/task";
+    }
     //
     @PostMapping("task/add")
     public String TaskPageCreate(@Valid Task task, BindingResult bindingResult){
@@ -25,29 +42,13 @@ public class ControllerTask  {
         taskRepository.save(task);
         return "redirect:/task/list";
     }
-    //
-    //
-    @GetMapping(value={"task/delete/{id}"})
-    public String HomePageID(Model modelHome, @PathVariable Long id) {
-        //modelHome.addAttribute("HomePage", id);
-        taskRepository.deleteById(id);
-        return "redirect:/task/list";
-    }
-    //
-    //
     @GetMapping("task/add")
     public String TaskPageAdd(Task task) {
         return "task/add";
     }
     //
-    @GetMapping("task")
-    public String TaskPage(Model modelTask) {
-        return "task/task";
-    }
-    //
     @GetMapping("task/list")
-    public String TaskPageList(Model modelTaskList)
-    {
+    public String TaskPageList(Model modelTaskList){
         modelTaskList.addAttribute("TaskList", taskRepository.findAll());
         return "task/list";
     }
